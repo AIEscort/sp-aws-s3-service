a6b0b87834c24ebd4f7e6c0cce5a69bc4c3e7c22
Volodymyr <vzahanych@gmail.com>
2023-10-15 16:54:28 +0200

    generate start app

---
+++ src/main/java/com/aiescort/s3/SpAwsS3ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aiescort.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAwsS3ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpAwsS3ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aiescort/s3/SpAwsS3ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aiescort.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpAwsS3ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
